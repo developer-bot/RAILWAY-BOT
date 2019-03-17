4dd79ee296dc0612af72ae23841ed82c4ae9ea53
Piush-hexaware <piushs.hexaware@gmail.com>
2019-03-17 23:16:05 +0530

    between station 18

--- handlers/trainBetweenStation.js
+++ handlers/trainBetweenStation.js
@@ -87,6 +87,15 @@ expObj.getTrainBetweenStation = function(req, res){
         ,'method':"GET"
         ,'body': null
         }
+        apiService.callAPI(details)
+        .then(resBody=>{
+            console.log("response body from api",JSON.stringify(resBody))
+        })
+        .catch(e =>{
+            console.log("error in api calling");
+             return sendCommonErrorResponse(req, res);
+         });
+
 }
 
 
@@ -118,134 +127,6 @@ function getStationCode(botData) {
     stationCodeArr.push(uDataTo.code)
     return stationCodeArr;
 }
-
-
-
-// expObj.getTrainBetweenStation = function(req, res){
-//     personalInfoLogs.info(`request body for emergrncy details ${JSON.stringify(req.body)}`)
-//     if (req.body.entityName.contactName == "") {
-//         let simpresponse = {"simpleMsgs":[
-//             {
-//                 "displayText": apiConfig.AddEmergencyContacts.ResMsg.ASK_CONTACT_NAME_TEXT
-//                 ,"textToSpeech":apiConfig.AddEmergencyContacts.ResMsg.ASK_CONTACT_NAME_SPEECH,
-//             }],
-//         "contextOut":""
-//         ,"sugChips":""
-//         };
-//         templateService.simpleResponse(simpresponse)
-//         .then(respObj=>{
-//             return res.json(respObj).end();
-//         }).catch (e =>{
-//             personalInfoLogs.error(e);
-//             sendCommonErrorResponse(req, res);
-//         });
-//     } else if (req.body.entityName.relationshipType == "") {
-//         askRelationshipType(req,res);
-//     } else if (req.body.entityName.phoneNumberType == "") {
-//         askEmergencyContactPhoneType(req,res);
-//     } else if (req.body.entityName.phoneNumber == "") {
-//         let simpresponse = {"simpleMsgs":[
-//             {
-//                 "displayText": apiConfig.AddEmergencyContacts.ResMsg.ASK_CONTACT_NUMBER_TEXT
-//                 ,"textToSpeech":apiConfig.AddEmergencyContacts.ResMsg.ASK_CONTACT_NUMBER_SPEECH,
-//             }],
-//         "contextOut":""
-//         ,"sugChips":""
-//         };
-//         templateService.simpleResponse(simpresponse)
-//         .then(respObj=>{
-//             return res.json(respObj).end();
-//         }).catch (e =>{
-//             personalInfoLogs.error(e);
-//             sendCommonErrorResponse(req, res);
-//         });
-//     } else if(req.body.entityName.preferredFlag == ""){
-//         askPrimaryContact(req,res);
-//     } else {
-//         let reqData = {
-//             //"EMPLID":req.body.userSession.employeeId,
-//             "EMPLID":"PU311",
-//             "CONTACT_NAME":req.body.entityName.contactName,
-//             "RELATIONSHIP":req.body.entityName.relationshipType,
-//             "PHONE_TYPE":req.body.entityName.phoneNumberType,
-//             "PHONE":req.body.entityName.phoneNumber,
-//             "PRIMARY_CNCT":req.body.entityName.preferredFlag
-//         };
-//         let postURL = apiConfig["AddEmergencyContacts"]["path"]
-//         personalInfoLogs.info(`url for add emergency contact ${postURL}`);
-//         personalInfoLogs.info(`EMERGENCY DETAILS BODY ${JSON.stringify(reqData)}`);
-//         let details={
-//         'url':postURL
-//         ,'method':"POST"
-//         ,'body': reqData
-//         ,'reqHeaders':req.headers
-//         };
-//         apiService.callAPI(details)
-//             .then(body =>{
-//                 personalInfoLogs.info("success /error msg form api ",body.HX_ESS_EMERGENCY_RESP);
-//                 if(body.HX_ESS_EMERGENCY_RESP) {
-//                     if(body.HX_ESS_EMERGENCY_RESP.MESSAGE && body.HX_ESS_EMERGENCY_RESP.MESSAGE.indexOf("success") != -1){
-//                         let relationshipType = STUDENT_DATA.emergencyContactRelationshipTypes[req.body.entityName.relationshipType] && STUDENT_DATA.emergencyContactRelationshipTypes[req.body.entityName.relationshipType].shortDesc ? STUDENT_DATA.emergencyContactRelationshipTypes[req.body.entityName.relationshipType].shortDesc : req.body.entityName.relationshipType;
-//                         let phoneNumberType = STUDENT_DATA.phoneNumberTypes[req.body.entityName.phoneNumberType] && STUDENT_DATA.phoneNumberTypes[req.body.entityName.phoneNumberType].description ? STUDENT_DATA.phoneNumberTypes[req.body.entityName.phoneNumberType].description : req.body.entityName.phoneNumberType;
-//                         personalInfoLogs.log('DATA EMGC ',reqData, relationshipType, phoneNumberType, req.body.entityName); 
-//                         let cardDetails = {
-//                         "simpleMsgs":[
-//                         {
-//                             "displayText": "Saved Successfully"
-//                             ,"textToSpeech":""
-//                         }],
-//                     "cardMsg": {
-//                         "title": "Contact Name : "+ req.body.entityName.contactName
-//                         ,"subTitle": "Relationship : "+ relationshipType
-//                         ,"formattedText": "**"+phoneNumberType + "** : " + req.body.entityName.phoneNumber
-//                         ,"url": ""
-//                         ,"altText": ""
-//                         , "url": ""
-//                         , "postback":""   
-//                     } 
-//                     ,"contextOut":""
-//                     ,"sugChips": COMMON_DATA['commonChipData']
-//                     }
-//                     personalInfoLogs.log("final response for dialog",cardDetails);
-//                         templateService.cardResponse(cardDetails)
-//                         .then(respObj=>{
-//                             for(let i=0; i < req.body.contexts.length; i++){
-//                                 req.body.contexts[i].lifespanCount = 0;
-//                             }
-//                             respObj.contextOut=req.body.contexts;
-//                             return res.json(respObj).end();
-//                         }).catch (e =>{
-//                             personalInfoLogs.error(e);
-//                             sendCommonErrorResponse(req, res);
-//                         });
-//                     } else{
-//                         personalInfoLogs.log(' FAIL IN ',body.HX_ESS_EMERGENCY_RESP);
-//                        let simpresponse = {"simpleMsgs":[
-//                         {
-//                             "displayText": body.HX_ESS_EMERGENCY_RESP.MESSAGE ? body.HX_ESS_EMERGENCY_RESP.MESSAGE : body.HX_ESS_EMERGENCY_RESP
-//                             ,"textToSpeech":body.HX_ESS_EMERGENCY_RESP ? body.HX_ESS_EMERGENCY_RESP.MESSAGE : body.HX_ESS_EMERGENCY_RESP
-//                         }],
-//                     "contextOut":""
-//                     ,"sugChips":COMMON_DATA['commonChipData']
-//                     };
-//                         templateService.simpleResponse(simpresponse)
-//                         .then(respObj=>{
-//                             return res.json(respObj).end();
-//                         }).catch (e =>{
-//                             personalInfoLogs.error(e);
-//                             sendCommonErrorResponse(req, res);
-//                         });
-//                     }
-//                 } else {
-//                     sendCommonErrorResponse(req, res);
-//                 }
-//             })
-//             .catch(err =>{
-//                 personalInfoLogs.error("ADD EMERG ERR ",reqData, err);
-//                 sendCommonErrorResponse(req, res);
-//             });
-//     }
-// };
 
 
 function sendCommonErrorResponse(req,res){
