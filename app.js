a3b3813db057e237fb285c545184325f425e1814
Piush-hexaware <piushs.hexaware@gmail.com>
2019-03-17 16:30:58 +0530

    between station 2

--- app.js
+++ app.js
@@ -4,37 +4,17 @@ var bodyParser 		= require('body-parser');
 //const PS_API = require('./config/psApi.json');
 var app 			= express();
 var port = process.env.PORT || 2020;
+let trainBetween = require('./handlers/trainBetweenStation.js');
 
 app.use(bodyParser.urlencoded({ extended: false })) 
 app.use(bodyParser.json());
 
 app.post("/webhook",function(req,res){
-    console.log("received a post request",JSON.stringify(req.body));
-      let responseObj=
-      {
-        "payload": {
-          "google": {
-            "expectUserResponse": true,
-            "richResponse": {
-              "items": [
-                {
-                  "simpleResponse": {
-                    "displayText": "Your pizza delivery **has** arrived! Thank you for using __Pizza Bot!__",
-                    "textToSpeech": "Your pizza delivery **has** arrived! Thank you for using __Pizza Bot!__"
-                  }
-                }
-              ]
-            }
-          }
-        }
-      }
-    return res.json(responseObj);
+    let body = req.body
     
-    });
-    
-  app.listen(port,function(){
-    console.log("railway bot is running successfully"+port);
-
+    if(body.queryResult.intent.displayName == "trainBetweenStation") {
+        trainBetween.getTrainBetweenStation(req,res)
+    }
 });
 process.on('unCaughtException', function(err){
 	console.log(err);
