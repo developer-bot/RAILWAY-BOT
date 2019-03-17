17bd580ea4642720b9a199945537613e70732e2e
Piush-hexaware <piushs.hexaware@gmail.com>
2019-03-17 23:54:24 +0530

    between station 20

--- utils/responseTemplate.js
+++ utils/responseTemplate.js
@@ -114,87 +114,57 @@ expObj.cardResponse = function(params){
 
 // List response -> Single msg, multiple msg, List with (or) without chips
 /* Params structure ->  
-{
-    "simpleMsgs":[
-    {
-        "displayText": ""
-        ,"textToSpeech":""
-    }],
-  "list": {
-    "itemValues":[
-        {
-          "title":""
-          ,"subTitle":""
-          ,"synonyms":""  
-          ,"url":""
-          ,"urlText":""
-          ,"postback":""
-          ,"buttonTitle":""
-          ,"buttonUrl":""
-        }
-    ]  
-  } 
-,"contextOut":""
-,"sugChips":[{
-            "title": "" 
-            ,"type": ""
-            ,"postback": ""
-            
-        }]
-
-}  
 */  
+// {
+//   "payload": {
+//     "google": {
+//       "expectUserResponse": true,
+//       "richResponse": {
+//         "items": [
+//           {
+//             "simpleResponse": {
+//               "textToSpeech": "Choose a item"
+//             }
+//           }
+//         ]
+//       },
+//       "systemIntent": {
+//         "intent": "actions.intent.OPTION",
+//         "data": {
+//           "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+//           "listSelect": {
+//             "title": "Hello",
+//             "items": [
+//               {
+//                 "optionInfo": {
+//                   "key": "first title key"
+//                 },
+//                 "description": "first description",
+//                 "image": {
+//                   "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+//                   "accessibilityText": "first alt"
+//                 },
+//                 "title": "first title"
+//               },
+//               {
+//                 "optionInfo": {
+//                   "key": "second"
+//                 },
+//                 "description": "second description",
+//                 "image": {
+//                   "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
+//                   "accessibilityText": "second alt"
+//                 },
+//                 "title": "second title"
+//               }
+//             ]
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
 
-
-{
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "Choose a item"
-            }
-          }
-        ]
-      },
-      "systemIntent": {
-        "intent": "actions.intent.OPTION",
-        "data": {
-          "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-          "listSelect": {
-            "title": "Hello",
-            "items": [
-              {
-                "optionInfo": {
-                  "key": "first title key"
-                },
-                "description": "first description",
-                "image": {
-                  "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-                  "accessibilityText": "first alt"
-                },
-                "title": "first title"
-              },
-              {
-                "optionInfo": {
-                  "key": "second"
-                },
-                "description": "second description",
-                "image": {
-                  "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
-                  "accessibilityText": "second alt"
-                },
-                "title": "second title"
-              }
-            ]
-          }
-        }
-      }
-    }
-  }
-}
 expObj.listResponse = function (params) {
     return new Promise((resolve, reject) => {
     var simple_msgs=[];
